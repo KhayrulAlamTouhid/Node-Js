5ddc75379baeac33eba77aad17e1ce69e3b597c1
KhayrulAlamTouhid <116265010+KhayrulAlamTouhid@users.noreply.github.com>
2022-10-27 16:39:00 +0600

    Add files via upload

---
+++ class 7/main.js
@@ -0,0 +1,3 @@
+const s1 = require('./function.js')
+console.log(s1.Name())
+console.log(s1.Address())
---
+++ class 8/vehicle.js
@@ -0,0 +1,13 @@
+const ImportCar = require('./car.js')
+const ImportBus = require('./bus.js')
+
+console.log(ImportCar.price(2555))
+console.log(ImportBus.price(2565))
+
+var obb ={
+    Name:'BMW',
+    model:'Unknown',
+    Height: '5.6m'
+}
+    
+console.log(ImportCar.details(obb))
